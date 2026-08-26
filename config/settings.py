a76a856d4d54478c8a01e7d7730d3f632b9b1778
maxim <mcsim55555k@gmail.com>
2026-08-26 13:41:19 +0300

    Homework 33 celery and redis settings

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,13 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+CELERY_TIMEZONE = "Moscow/Europe"
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
